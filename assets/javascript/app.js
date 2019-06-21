9d6ca13885e2a7f19e23b28d3649185b9ae65c93
Thomas Roman <thomasroman32@yahoo.com>
2019-06-20 23:52:33 -0500

    created the ajax callinside of the start button function that iterates response results array.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -66,6 +66,64 @@ $("#start-button").on("click", function () {
 
     $(".container").append(timerDiv);
 
+
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+
+        for (var i = 0; i < response.results.length; i++) {
+
+            var answersArr = response.results[i].incorrect_answers;
+            answersArr.push(response.results[i].correct_answer);
+            correctAnswers.push(response.results[i].correct_answer);
+
+            answersArr = shuffle(answersArr);
+
+
+            var questionDiv = $("<div>");
+            questionDiv.attr("class", "row question");
+            questionDiv.html(`<div class="col">
+            <h2>${response.results[i].question}</h2>
+            <div class="form-check-inline">
+                <input class="form-check-input" type="radio" name="question${i + 1}" id="q${i + 1}a1" value="${answersArr[0]}">
+                <label class="form-check-label" for="q${i + 1}a1">
+                    ${answersArr[0]}
+                </label>
+            </div>
+            <div class="form-check-inline">
+                <input class="form-check-input" type="radio" name="question${i + 1}" id="q${i + 1}a2" value="${answersArr[1]}">
+                <label class="form-check-label" for="q${i + 1}a2">
+                    ${answersArr[1]}
+                </label>
+            </div>
+            <div class="form-check-inline">
+                <input class="form-check-input" type="radio" name="question${i + 1}" id="q${i + 1}a3" value="${answersArr[2]}">
+                <label class="form-check-label" for="q${i + 1}a3">
+                    ${answersArr[2]}
+                </label>
+            </div>
+            <div class="form-check-inline">
+                <input class="form-check-input" type="radio" name="question${i + 1}" id="q${i + 1}a4" value="${answersArr[3]}">
+                <label class="form-check-label" for="q${i + 1}a4">
+                    ${answersArr[3]}
+                </label>
+            </div>
+        </div>`)
+
+            $(".container").append(questionDiv);
+        }
+
+        var doneButton = $(`<div class="row" id="done-button-row">
+        <div class="col">
+            <button type="button" id="done-button">Done</button>
+        </div>
+    </div>`);
+    $(".continer").append(doneButton);
+    })
+        
+
     timer.start();
 });
 
@@ -84,4 +142,12 @@ function endGame() {
             unanswered--;
         }
     }
+    $(".question").remove();
+    $("#done-button-row").remove();
+    $("#timer-box").remove();
+
+    $(".container").append(`<h2 style='margin-bottom: 1em;'>All Done!</h2>`);
+    $(".container").append(`<p>Correct Answers: ${correct}</p>`);
+    $(".container").append(`<p>Incorrect Answers: ${incorrect}</p>`);
+    $(".container").append(`<p>Unanswered: ${unanswered}</p>`);
 }
